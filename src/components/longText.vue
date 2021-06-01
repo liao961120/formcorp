liao961120/formcorp

commit dade7be27ecc334fc4c47da8f4e7ebb6b3b6336f

add file info on topbar

src/components/longText.vue
@@ -19,9 +19,12 @@
         <v-icon>mdi-menu</v-icon>
       </v-btn> -->
 
+      <span style="position:fixed;left:1rem;top:1rem;color:grey">
+        {{ text.meta.video.split('.')[0] }}
+      </span>
       <v-row justify="center" align="center">
         <v-spacer class="d-none d-md-flex"></v-spacer>
-        <v-col cols="6" class="d-none d-sm-flex justify-center grey--text text-darken-3 text-h6">
+        <v-col cols="5" class="d-none d-sm-flex justify-center grey--text text-darken-3 text-h6">
           <template>
             {{ text.meta.language.split(', ')[0] }}
           </template>

commit 78f790bb5a1bcd60e74d847d7941cc587c88640b

fix bug introduced in dade7be27e

src/components/longText.vue
@@ -19,8 +19,8 @@
         <v-icon>mdi-menu</v-icon>
       </v-btn> -->
 
-      <span style="position:fixed;left:1rem;top:1rem;color:grey">
-        {{ text.meta.video.split('.')[0] }}
+      <span style="position:fixed;right:1rem;top:1rem;color:grey">
+        {{ `${filename.split('/').pop()}.txt` }}
       </span>
       <v-row justify="center" align="center">
         <v-spacer class="d-none d-md-flex"></v-spacer>
@@ -57,6 +57,7 @@ export default {
     return {
       text: {},
       src: `https://yongfu.name/glossParser/${this.$route.params.id}.json`,
+      filename: `${this.$route.params.id}`,
       currGroupNum: false,
       lastSentEndNum: [],
       hash: this.$route.hash,