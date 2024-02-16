178ab1ddd94c5503231849e9cba8079b7a008926
Yongfu Liao <liao961120@gmail.com>
2024-02-16 09:15:54 +0800

    prepare for http deploy

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/liao961120/formcorp.git master:web
+git push -f https://github.com/liao961120/formcorp.git master:gh-pages
+
+touch .nojekyll
+echo "formcorp.yongfu.name" > CNAME
 
 cd -
