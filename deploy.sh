daa4f61c490a60152b73d069a8bab171b2e11294
Yongfu <liao961120@gmail.com>
2024-12-09 20:23:52 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,15 @@ set -e
 npm run build
 
 cd dist
+rm -rf .git
+
+touch .nojekyll
+echo "corpus.linguistics.ntu.edu.tw" > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 git push -f https://github.com/liao961120/formcorp.git master:gh-pages
 
-touch .nojekyll
-echo "corpus.linguistics.ntu.edu.tw" > CNAME
 
 cd -
